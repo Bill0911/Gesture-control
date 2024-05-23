01010d921e7b35074d0c9610b9eb950a46ed85da
Nguyendo1234 <do.do@student.nhlstenden.com>
2024-05-23 20:04:07 +0200

    New feature (turn off webcam) released

--- Demo.py
+++ Demo.py
@@ -1,107 +1,80 @@
-import cv2 
-import mediapipe as mp 
-from math import hypot 
-import screen_brightness_control as sbc 
+# Import the required modules
+import cv2
+import mediapipe as mp
+from math import hypot
+import screen_brightness_control as sbc
 import numpy as np
 import pyautogui
 import time
 
-# creating the hand recognizer model 
+# Create the hand recognizer model
 mpHands = mp.solutions.hands
-hands = mpHands.Hands( 
-    static_image_mode=False, 
-    model_complexity=1, 
-    min_detection_confidence=0.75, 
-    min_tracking_confidence=0.75, 
+hands = mpHands.Hands(
+    static_image_mode=False,
+    model_complexity=1,
+    min_detection_confidence=0.75,
+    min_tracking_confidence=0.75,
     max_num_hands=2)
 
-Draw = mp.solutions.drawing_utils 
+Draw = mp.solutions.drawing_utils
 
-# Starting webcam to capture 
-cap = cv2.VideoCapture(0) 
+# Start the webcam to capture video
+cap = cv2.VideoCapture(0)
 
-#For scroll up-down feature.
-#Initialize variables
+# Initialize variables for the scroll up-down feature
 last_scroll_time = 0
-scroll_coolDown = 0.5 
-min_scroll_distance = 0.05 
+scroll_coolDown = 1.5
+min_scroll_distance = 0.05
 last_index_y = None
+last_tab_time = 0
+tab_coolDown = 1.5
+new_tab_opened = False
 
-#For brightness adjustment feature.
-#Initialize variables
+# Initialize variables for the brightness adjustment feature
 last_brightness_time = None
-brightness_adjustment_coolDown = 3
+
 last_switch = time.time()
-is_adjusting_brightness = False
-is_switching_tabs = False
 
-while True: 
-    # Read video frame 
-    _, frame = cap.read() 
+while True:
+    # Read the video frame
+    _, frame = cap.read()
 
-    # Flip image 
-    frame = cv2.flip(frame, 1) 
+    # Flip the image
+    frame = cv2.flip(frame, 1)
 
-    # Convert BGR image to red-green-blue image (OpenCV) 
-    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+    # Convert the BGR image to RGB
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Processing red-green-blue image 
+    # Process the RGB image
     Process = hands.process(frameRGB)
 
-    landmarkList = [] 
-    # if hands are present in image(by frame) 
-    if Process.multi_hand_landmarks: 
-        # recognize handmarks 
-        for handlm in Process.multi_hand_landmarks: 
-            for _id, landmarks in enumerate(handlm.landmark): 
-                # store height and width of image 
-                height, width, color_channels = frame.shape 
+    landmarkList = []
+    # If hands are detected in the image
+    if Process.multi_hand_landmarks:
+        # Recognize hand landmarks
+        for handlm in Process.multi_hand_landmarks:
+            # Get the handedness of the detected hand
+            handedness = Process.multi_handedness[0].classification[0].label
+            # Only process the landmarks if the detected hand is the right hand
+            if handedness == 'Right':
+                for _id, landmarks in enumerate(handlm.landmark):
+                    # Store the height and width of the image
+                    height, width, color_channels = frame.shape
 
-                # calculate & append x, y coordinates 
-                # of handmarks from image(frame) to lmList 
-                x, y = int(landmarks.x*width), int(landmarks.y*height) 
-                landmarkList.append([_id, x, y]) 
+                    # Calculate and append the x, y coordinates of the hand landmarks to the list
+                    x, y = int(landmarks.x * width), int(landmarks.y * height)
+                    landmarkList.append([_id, x, y])
 
-            # draw landmarks 
-            Draw.draw_landmarks(frame, handlm, 
-                                mpHands.HAND_CONNECTIONS) 
+                # Draw the hand landmarks
+                Draw.draw_landmarks(frame, handlm, mpHands.HAND_CONNECTIONS)
 
-    # If landmarks list !empty() 
-    if landmarkList != []: 
-        # store x,y coordinates of tip of thumb 
-        x_1, y_1 = landmarkList[4][1], landmarkList[4][2] 
-
-        # store x,y coordinates of tip of index finger 
-        x_2, y_2 = landmarkList[8][1], landmarkList[8][2] 
-
-        # draw circle on thumb and index finger tip 
-        cv2.circle(frame, (x_1, y_1), 7, (0, 255, 0), cv2.FILLED) 
-        cv2.circle(frame, (x_2, y_2), 7, (0, 255, 0), cv2.FILLED) 
-
-        # draw line from tip of thumb to tip of index finger 
-        cv2.line(frame, (x_1, y_1), (x_2, y_2), (0, 255, 0), 3) 
-
-        # calculate square root of the sum of 
-        # squares of the specified arguments. 
-        L = hypot(x_2-x_1, y_2-y_1) 
-
-        # 1-D linear interpolant to a function 
-        # with given discrete data points 
-        # (Hand range 15 - 220, Brightness 
-        # range 0 - 100), evaluated at length. 
-        b_level = np.interp(L, [15, 220], [0, 100]) 
-     
-    # set up brightness
-     
-        if last_brightness_time is None:
-            last_brightness_time = time.time()
-        elif time.time() - last_brightness_time:
-            sbc.set_brightness(int(b_level))
-            last_brightness_time = time.time()
-        
-        #Scrolling
+    # If the landmarks list is not empty
+    if landmarkList != []:
+        # Scroll up or down
         if len(landmarkList) >= 21:  # Make sure the index finger tip and middle finger tip are detected
             index_tip = landmarkList[8]
+            thumb_tip = landmarkList[4]
+            thumb_base = landmarkList[2]
             index_pip = landmarkList[6]
             pinky_tip = landmarkList[20]
             pinky_pip = landmarkList[18]
@@ -112,28 +85,31 @@ while True:
 
             if last_index_y is None:
                 last_index_y = index_tip[2]
-    
+
             if time.time() - last_scroll_time > scroll_coolDown:
                 if index_tip[2] < middle_tip[2] and abs(index_tip[2] - last_index_y) > min_scroll_distance:
                     print("Scrolling up")
-                    pyautogui.scroll(180)
+                    pyautogui.scroll(380)
                     last_scroll_time = time.time()
-            elif index_tip[2] < pinky_tip[2] and pinky_tip[2] < pinky_pip[2] and abs(index_tip[2] - last_index_y) > min_scroll_distance:
-                    print("Scrolling down")
-                    pyautogui.scroll(-50)
-                    last_scroll_time = time.time()
-            elif ring_tip[2] < ring_pip[2] and ring_tip[2] > index_tip[2]:
-                    print("switch tabs")
-                    pyautogui.keyDown('ctrl')
-                    # Press 'tab'
-                    pyautogui.press('tab')
-                    # Release 'ctrl'
-                    pyautogui.keyUp('ctrl')
-                    # Update the time of the last tab switch
-                    last_switch = time.time()
-        last_index_y = index_tip[2]
+            elif thumb_tip[2] < index_tip[2] and abs(index_tip[2] - last_index_y) > min_scroll_distance:
+                print("Scrolling down")
+                pyautogui.scroll(-50)
+                last_scroll_time = time.time()
+            elif pinky_tip[2] < pinky_pip[2] and pinky_tip[2] > index_tip[2]:
+                print("Switching tabs")
+                pyautogui.keyDown('ctrl')
+                pyautogui.press('tab')
+                pyautogui.keyUp('ctrl')
+                time.sleep(1)
+                last_switch = time.time()
+            elif thumb_tip[2] > thumb_base[2] and index_tip[2] < thumb_tip[2]:
+                print("Turning off webcam")
+                break
             
-    #If the user wants to turn off the webcam, press 'q'
-    cv2.imshow('Image', frame) 
-    if cv2.waitKey(1) & 0xff == ord('q'): 
+
+        last_index_y = index_tip[2]
+
+    # If the user wants to turn off the webcam, press 'q'
+    cv2.imshow('Image', frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):
         break
